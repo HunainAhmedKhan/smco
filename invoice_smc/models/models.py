29fbaf89737d36ebd2e86d79e5505cb2776fc787
smcviltco <75363152+smcviltco@users.noreply.github.com>
2020-12-31 16:40:32 +0500

    Add files via upload

---
+++ invoice_smc/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class invone_smc(models.Model):
+    _inherit = 'res.partner'
+
+    no_cnic = fields.Char(string='CNIC')
+    ntn = fields.Char(string='NTN')
+
+
+class InheritField(models.Model):
+    _inherit = 'account.move'
+
+    freight = fields.Char(string='Freight')
+
+
