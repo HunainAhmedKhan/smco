3d0cd83cfbdb13499949fed98a647a5c8a654bb2
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2021-01-06 15:38:26 +0500

    Update models.py

--- smc_project/models/models.py
+++ smc_project/models/models.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api,_
-from odoo.exceptions import UserError
-
-
-
 class smc(models.Model):
     _inherit = 'product.template'
 
@@ -23,24 +16,40 @@ class smc(models.Model):
 class smc(models.Model):
     _inherit = 'stock.picking'
 
-    invoice_status = fields.Selection([('paid', 'Paid'), ('not_paid', 'Not Paid')
-                                 ], string="Invoice Status", compute="_check_status")
+    invoice_status = fields.Selection([('paid', 'Paid'), ('not_paid', 'Not Paid')], string="Invoice Status", compute="_check_status")
 
     def _check_status(self):
         for i in self:
             search_invoice = self.env['account.move'].search([('invoice_origin', '=', i.origin)], limit=1)
-            if search_invoice.payment_state in ['paid','partial','in_payment']:
+            if search_invoice.invoice_payment_state=='paid':
                 i.invoice_status="paid"
             else:
                 i.invoice_status = "not_paid"
-
-
 
 
 class in_invoicing(models.Model):
     _inherit = 'account.move'
 
     delivery_order = fields.Char(string='DO Number',compute='_compute_global')
+
+    check_status = fields.Selection([('credit_approval', 'Ask for Credit Approval'), ('approved', 'Credit Approved')], string="Check Status", readonly=True,compute="Check_status_payment")
+    state = fields.Selection(selection_add=[('credit_approval', 'Ask for Credit Approval'), ('approved', 'Credit Approved')])
+
+    def action_approve(self):
+            self.state = 'approved'
+
+    def Check_status_payment(self):
+        for i in self:
+            print(i.state)
+            if i.state in ['approved']:
+                i.check_status='approved'
+            else:
+                i.check_status ='credit_approval'
+
+
+
+    def action_ask_for_approval(self):
+        self.state ='credit_approval'
 
     def _compute_global(self):
         for i in self:
@@ -52,8 +61,11 @@ class in_invoicing(models.Model):
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    
-    
+
+
+    payment_status = fields.Selection([('paid', 'Paid'), ('not_paid', 'Not Paid')], string="Payment Status", compute="_check_status")
+
+
 
     state = fields.Selection([
         ('draft', 'Quotation'),
@@ -67,27 +79,27 @@ class SaleOrder(models.Model):
     max_discount = fields.Float(string='Max Disccount', compute='compute_max_disccount', default=0)
 
     allowed_discount = fields.Float(string='Allowed Disccount', related='user_id.allowed_discount')
-    user_id = fields.Many2one('res.users', string='User' ,compute="compute_self_id")
-    payment_status = fields.Selection([('paid', 'Paid'), ('not_paid', 'Not Paid')], string="Invoice Status", compute="_check_status")
-    
+    user_id = fields.Many2one('res.users', string='User',compute="compute_self_id")
+
+
     def compute_self_id(self):
-        for i in self:
-            i.user_id=self.env.uid
-
-
-    def _check_status(self):
-        for i in self:
-            search_invoice = self.env['account.move'].search([('invoice_origin', '=', i.name)], limit=1)
-            if search_invoice.payment_state in ['paid','partial','in_payment']:
-                i.payment_status="paid"
-            else:
-                i.payment_status = "not_paid"
+        self.user_id=self.env.uid
 
     def from_manager_approval(self):
         self.state='manager'
 
     def from_ceo_approval(self):
         self.state='ceo'
+
+    def _check_status(self):
+        for i in self:
+            search_invoice = self.env['account.move'].search([('invoice_origin', '=', i.name)], limit=1)
+            print("search_invoice.state ",search_invoice.state)
+            print(search_invoice.invoice_origin)
+            if search_invoice.payment_state in ['paid','partial'] or search_invoice.check_status in ['approved']:
+                i.payment_status="paid"
+            else:
+                i.payment_status = "not_paid"
 
 
 
@@ -109,19 +121,12 @@ class SaleOrder(models.Model):
             maximum = []
             for rec in i.order_line:
                 maximum.append(rec.discount)
-            diss = max(maximum)
+                diss = max(maximum)
             i.max_discount = diss
-
-
-
 
 
 class users_inherit(models.Model):
     _inherit = 'res.users'
     _description = 'adding to users table'
 
-
-
     allowed_discount = fields.Float(string='Discount Allowed')
-
-
